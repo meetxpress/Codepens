704f540cf92ce8ac2c41ecf24acc4529eeabd7ed
Meet Patel <pmeet7895@gmail.com>
2020-10-17 12:40:38 +0530

    Create main.js

---
+++ Login Form with Hidden Fields/main.js
@@ -0,0 +1,7 @@
+function lol() {
+	if((username.value.length == "") || (password.value.length == "")){
+		alert("One of the field is empty. Please enter value in both the fields.");	
+	}else{
+        alert("Incorrect Password. Please stop entering your ex's name as a password.");		
+    }		
+}
